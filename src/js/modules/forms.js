9b545c574d6494142760d651b5e2da1197dc623b
Андрей Дубровский <Dronandre@users.noreply.github.com>
2020-08-21 12:33:18 +0700

    add timer

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -40,6 +40,7 @@ const forms = (state) => {
             item.appendChild(statusMessge);
 
             const formData = new FormData(item);
+            
             if (item.getAttribute('data-calc') === "end") {
                 for(let key in state){
                     formData.append(key, state[key]);
